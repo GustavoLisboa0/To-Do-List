5af7891ff3d9a11a22f4e463ef4b8ab1a9f49592
GustavoLisboa0 <gustavo20031010@gmail.com>
2024-03-26 19:23:36 -0300

    manutenção

--- To do List/app.js
+++ To do List/app.js
@@ -1,46 +1,67 @@
 function adicionaTarefaNaLista() {
     const novaTarefa = document.getElementById('input_nova_tarefa').value;
-
     if (novaTarefa.trim() !== '') {
         criaNovoItemDaLista(novaTarefa);
-        document.getElementById('input_nova_tarefa').value = ''; 
+        document.getElementById('input_nova_tarefa').value = '';
     }
 }
 
 function criaNovoItemDaLista(textoDaTarefa) {
     const listaTarefas = document.getElementById('lista_de_tarefas');
-    
     let qtdTarefas = listaTarefas.children.length;
-
     const novoItem = document.createElement('li');
-
     novoItem.innerText = textoDaTarefa;
-    
     novoItem.id = `tarefa_id_${qtdTarefas++}`;
 
-    novoItem.appendChild(criaInputCheckBoxTarefa(novoItem.id));
+    // Adiciona evento de duplo clique para editar
+    novoItem.addEventListener('dblclick', function() {
+        editaTarefa(this);
+    });
 
+    novoItem.appendChild(criaInputCheckBoxTarefa(novoItem.id));
     listaTarefas.appendChild(novoItem);
+}
+
+function editaTarefa(tarefa) {
+    const textoTarefa = tarefa.innerText;
+    const inputEdicao = document.createElement('input');
+    inputEdicao.type = 'text';
+    inputEdicao.value = textoTarefa;
+
+    inputEdicao.addEventListener('keypress', function(e) {
+        if (e.key === 'Enter') {
+            finalizaEdicaoTarefa(tarefa, inputEdicao);
+        }
+    });
+
+    tarefa.innerHTML = '';
+    tarefa.appendChild(inputEdicao);
+    inputEdicao.focus();
+}
+
+function finalizaEdicaoTarefa(tarefa, inputEdicao) {
+    const novoTexto = inputEdicao.value;
+    tarefa.innerHTML = novoTexto;
+    // Adiciona evento de duplo clique novamente para permitir futuras edições
+    tarefa.addEventListener('dblclick', function() {
+        editaTarefa(this);
+    });
+    tarefa.appendChild(criaInputCheckBoxTarefa(tarefa.id));
 }
 
 function criaInputCheckBoxTarefa(idTarefa) {
     const inputTarefa = document.createElement('input');
-    
     inputTarefa.type = 'checkbox';
-    
     inputTarefa.setAttribute('onclick', `mudaEstadoTarefa('${idTarefa}')`);
-
     return inputTarefa;
 }
 
 function mudaEstadoTarefa(idTarefa) {
     const tarefaSelecionada = document.getElementById(idTarefa);
-
-    if (tarefaSelecionada.style.textDecoration == 'line-through') {
-        tarefaSelecionada.style = 'text-decoration: none;';
-    } 
-    else {
-        tarefaSelecionada.style = 'text-decoration: line-through;';
+    if (tarefaSelecionada.style.textDecoration === 'line-through') {
+        tarefaSelecionada.style.textDecoration = 'none';
+    } else {
+        tarefaSelecionada.style.textDecoration = 'line-through';
     }
 }
 
@@ -54,4 +75,3 @@ function resetTarefas() {
         }
     });
 }
-
